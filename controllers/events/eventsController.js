91932366c31fd6dbfbba861f8084eeae935a15ab
DynamikArray <dynamikarray@gmail.com>
2021-12-19 01:02:44 -0500

    added eventsList call to sportsdata

--- controllers/events/eventsController.js
+++ controllers/events/eventsController.js
@@ -1,9 +1,9 @@
 const sportsDataService = require("../../services/sportsDataService");
 
 async function getEventsList(req, res) {
-  const items = await sportsDataService.getEventsList();
-
-  res.send(items);
+  const eventYear = 2021;
+  const eventsList = await sportsDataService.getEventsList(eventYear);
+  res.send(eventsList);
 }
 
 module.exports = { getEventsList };
--- services/sportsDataService.js
+++ services/sportsDataService.js
@@ -1,7 +1,33 @@
+require("dotenv").config();
+const APIURL = process.env.SPORTSDATA_URL;
+const APIKEY = process.env.SPORTSDATA_API;
+const APIHEADER = process.env.SPORTSDATA_AUTH_HEADER;
+
+const Axios = require("axios");
+const axios = Axios.create({
+  baseURL: APIURL,
+  timeout: 1000,
+  headers: { [APIHEADER]: [APIKEY] },
+});
+
 const sportsData = {
-  getEventsList: async function () {
-    const items = [{ data: "values" }];
-    return items;
+  getEventsList: async function (numberYear) {
+    try {
+      const scheduleResponse = await axios.get(
+        `/scores/json/Schedule/ufc/${numberYear}`
+      );
+
+      if (scheduleResponse.data) {
+        const items = scheduleResponse.data.slice().sort((a, b) => {
+          return new Date(b.DateTime) - new Date(a.DateTime);
+        });
+        return items;
+      }
+      return [];
+    } catch (e) {
+      console.log(e.response.data);
+      return false;
+    }
   },
 };
 
